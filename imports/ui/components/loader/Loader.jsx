fbd0fae028ec951b94bd18aadb42dc2dcad55659
Dmitry Tsepelev <dmitry.tsepelev@gmail.com>
2016-05-16 19:28:00 +0300

    Added loader

---
+++ imports/ui/components/loader/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { styles } from './loader.mss'
+import classNames from 'classnames'
+
+const classes = {
+  main: classNames('col-xs-12', styles.main, 'masonryItem'),
+}
+
+export const Loader = () => (
+  <div className={classes.main}>
+    Загружаем...
+  </div>
+)
